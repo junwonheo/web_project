d5fa6366ac3aecf853b811526e0d348f1ae514ea
yeeun <dldpdms0000@naver.com>
2023-08-24 12:40:41 +0900

    php

---
+++ php/login.php
@@ -0,0 +1,30 @@
+<?php
+$host="localhost";
+$user="root";
+$pw="";
+$db="wp";
+
+$conn=new mysqli($host,$user,$pw,$db);
+if($conn->connect_error){
+   echo "접속실패";
+}
+else {echo"접속 성공<br>";
+    if (isset($_POST['userid'])) {
+        $username=$_POST["userid"];
+        $password=$_POST["userpw"];
+        $sql = mysqli_query($conn,"select * from user where user_id='$username'");
+        $member = $sql->fetch_array();
+        $hash_pwd = $member['password'];
+        echo "hi";
+        if (password_verify($password, $hash_pwd)) {
+            $_SESSION['id'] = $member["id"];
+            $_SESSION['name'] = $member["name"];
+        
+            echo "<script>alert('로그인 성공!'); location.href='./index.html';</script>";
+        } else {
+            echo "아이디 또는 비밀번호를 확인하세요";
+        }
+    }
+}
+
+?>
---
+++ php/sign_up.php
@@ -0,0 +1,52 @@
+<?php
+$host="localhost";
+$user="root";
+$pw="";
+$db="wp";
+
+$conn=new mysqli($host,$user,$pw,$db);
+if($conn->connect_error){
+   echo "접속실패";
+}
+else {echo"접속 성공<br>";
+    if (isset($_POST['useremail'])) {
+        $useremail=$_POST["useremail"];
+        $username=$_POST["userid"];
+        $password=$_POST["userpw"];
+        $password2=$_POST["userpw2"];
+        
+        if($password2==$password){
+            echo "비밀번호 통과<br>";
+            $sql = mysqli_query($conn, "SELECT EXISTS (SELECT * from user WHERE user_email='$useremail') as success");
+            $useremailcount = $sql->fetch_assoc();
+            if($useremailcount['success']==1) {
+                echo "<script>alert('존재하는 이메일입니다.');</script>";
+            }
+            else{
+                echo "이메일 통과<br>";
+                $sql = mysqli_query($conn, "SELECT EXISTS (SELECT * from user WHERE user_id='$username') as success");
+                $usernamecount = $sql->fetch_assoc();
+                if($usernamecount['success']==1) {
+                    echo "<script>alert('존재하는 아이디입니다.');</script>";
+                }
+                
+                else{
+                    $password = password_hash($password, PASSWORD_DEFAULT);
+                    $sql="INSERT INTO user(user_email, user_id, user_pw) VALUES('$useremail','$username','$password')";
+                    if($conn->query($sql)){
+                        echo "successfully";
+                    }
+                    else{
+                        echo "erro";
+                    
+                    }
+                }
+            }
+        }
+        else{
+            echo "<script>alert('비밀번호가 다릅니다.');</script>";
+        }
+    }
+}
+
+?>
